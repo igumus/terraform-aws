2dda99ddb57db172a5115d1f905f33c89a1b92e0
igumus <ibrhm.gumus@gmail.com>
2023-11-09 13:08:46 +0300

    renamed: vpc.tf to securityGroup.tf in 002-vpc project

--- 002-vpc/main.tf
+++ 002-vpc/main.tf
@@ -2,6 +2,16 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
--- 002-vpc/securityGroup.tf
+++ 002-vpc/securityGroup.tf
@@ -1,13 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP inbound traffic"
